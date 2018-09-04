3b34fee0a5dc55b3da0ec4d2bb0e743c6e074194
FabianM <42346263+fabianm1705@users.noreply.github.com>
2018-09-04 14:22:12 -0300

    Add files via upload

---
+++ loginZonaSocios.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./images/flor_amparo.ico">
+  <link rel="icon" type="image/png" href="./images/flor_amparo.ico">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Inicio de Sesión... - Amparo Servicios Sociales
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="./assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+</head>
+
+<body class="login-page sidebar-collapse">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate" id="ocultodecelu">
+        <a class="navbar-brand" href="index.html">
+          Amparo Servicios Sociales </a>
+<!--        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">probando</span>
+          <span class="navbar-toggler-icon">probando 2</span>
+          <span class="navbar-toggler-icon">probando 3</span>
+          <span class="navbar-toggler-icon"></span>
+        </button>-->
+      </div>
+      <div class="collapse navbar-collapse" id="ocultodecelu">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/amparosrl" target="_blank" data-original-title="Danos un Me Gusta en Facebook">
+              <i class="fa fa-facebook-square"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="page-header header-filter" style="background-image: url('./images/35.jpg'); background-size: cover; background-position: top center;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+          <div class="card card-login">
+            <form class="form" method="post" action="contenedorPagina.php" id="formUsuario" name="formUsuario">
+              <div class="card-header card-header-primary text-center">
+                <h4 class="card-title">Inicio de Sesión...</h4>
+                <div class="social-line">
+                  <a href="https://www.facebook.com/amparosrl" class="btn btn-just-icon btn-link">
+                    <i class="fa fa-facebook-square"></i>
+                  </a>
+                  <a href="#pablo" class="btn btn-just-icon btn-link">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                  <a href="#pablo" class="btn btn-just-icon btn-link">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </div>
+              </div>
+              <p class="description text-center">Amparo Servicios Sociales</p>
+              <div class="card-body">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">face</i>
+                    </span>
+                  </div>
+                  <input type="text" class="form-control" name="login"  id="login" placeholder="Número de Socio..." required>
+                </div>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">mail</i>
+                    </span>
+                  </div>
+                  <input id="correo"  name="correo" type="email" class="form-control" placeholder="Email...">
+                </div>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">lock_outline</i>
+                    </span>
+                  </div>
+                  <input id="password"  name="password" type="password" class="form-control" placeholder="Contraseña..." required>
+                  <input name="deDonde" type="hidden" value="pagina" />
+                </div>
+              </div>
+              <div class="footer text-center">
+                <a href="#pablo" class="btn btn-primary btn-link btn-wd btn-lg">Iniciar!</a>
+              </div>
+            </form>
+            <?php
+               $deDonde="pagina";
+               if (isset($_GET['mostrar'])){
+               $mostrar="visible";
+            ?>
+            <script language="javascript">cambiarTamanioDiv("60%")</script>
+            <?php
+               }else{$mostrar="noVisible";}
+            ?>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="container" id="ocultodecelu">
+        <div class="copyright float-right">
+          &copy;
+          <script>
+            document.write(new Date().getFullYear())
+            </script>, <b><a href="https://www.amparosrl.com.ar" target="_blank">Amparo</a></b> Cura Alvarez 615 - Tel.:4235108
+          </div>
+      </div>
+    </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="./assets/js/plugins/moment.min.js"></script>
+  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="./assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--	Plugin for Sharrre btn -->
+  <script src="./assets/js/plugins/jquery.sharrre.js" type="text/javascript"></script>
+  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
+</body>
+</html>
